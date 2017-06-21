72725eeb4fcfcd2a1b27077a97a87019cb40aa50
swathi reddy nursing <swathi reddy nursing>
2017-06-21 00:05:54 -0400

    add

---
+++ day-1/app/idbased.js
@@ -0,0 +1,16 @@
+(function(){
+    
+   var lblFirstName= $('#lblFirstName');
+   
+    
+    //Assigning the value to the text box.
+    
+    $("#txtFirstName").val("JQuery");
+    
+    var lastName = $("#txtLastName").val();
+    console.log(lastName);
+    
+    
+  
+    
+})();
